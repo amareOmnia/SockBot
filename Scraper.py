24555cd767a43313801f1ea25b5868f0b9027707
Cooper Lewis <thatcooperlewis@gmail.com>
2018-10-19 11:27:38 -0400

    added login example

--- Scraper.py
+++ Scraper.py
@@ -3,33 +3,38 @@
 class collect_sub:
 
     import praw
-    from login_credentials import Login
-    lg = Login()
+    import login_credentials as lg
+
+    print ('scraper username: /u/' + lg.get_username())
+
     # login information for Reddit account is hidden in a gitignore file.
-    # See 'login_template.py' for formatting guidelines
-    reddit = praw.Reddit(client_id=lg.get_client_id,
-                        client_secret=lg.get_client_secret,
-                        password=lg.get_password,
-                        user_agent=lg.get_user_agent,
-                        username=lg.get_username)
+    # See 'login_example.py' for formatting guidelines
+    reddit = praw.Reddit(client_id=lg.get_client_id(),
+                        client_secret=lg.get_client_secret(),
+                        password=lg.get_password(),
+                        user_agent=lg.get_user_agent(),
+                        username=lg.get_username())
+
 
     the_self = reddit.user.me()
     print('Hi! This is', the_self, 'an imma bout da scrape some philosophical scalps.')
-    print(reddit.read_only)
+    # print(reddit.read_only)
     ''' maybe add input for desired sub_reddit: /r/____'''
 
     '''declares subreddit'''
-    sub_reddit = reddit.subreddit("askphilosophy")
+    sub_reddit = reddit.subreddit("philosophy")
 
     '''collects top submissions'''
     comments_list = list()
-    for submission in sub_reddit.top(time_filter='year', limit=1):
+    for submission in sub_reddit.top(time_filter='month', limit=1):
         print('Scanning:', submission.title, 'ID:', submission.id)
 
         '''collects hot comments from them'''
         submission.comments.replace_more(limit=None)
         for comment in submission.comments.list():
             print('[', comment.author, ']:', comment.body)
+            if comment.author.toString() == "None":
+                continue
             comments_list.append(comment)
 
             comment_queue = submission.comments
---
+++ login_example.py
@@ -0,0 +1,21 @@
+# extremely simple script for providing login information
+def get_client_id():
+  return 'CLIENT ID GOES HERE'
+  
+
+def get_client_secret():
+  return 'CLIENT SECRET GOES HERE'
+  
+
+def get_password():
+  return 'USER PASSWORD GOES HERE'
+  
+
+def get_user_agent():
+  return 'ARBITRARY DESCRIPTOR'
+  
+
+def get_username():
+  return 'USERNAME GOES HERE'
+  
+
